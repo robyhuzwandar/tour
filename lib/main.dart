aa7d1a0fdab2c09f209e5a5e54cebe0db4a9c4f1
Roby Huzwandar <robyhuzwandar@gmail.com>
2019-08-22 22:52:04 +0800

    Revert "cange readme"

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,54 @@
-import 'package:chewie/chewie.dart';
-import 'package:chewie/src/chewie_player.dart';
+import 'package:custom_chewie/custom_chewie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(
-    ChewieDemo(),
+    new ChewieDemo(),
   );
 }
 
 class ChewieDemo extends StatefulWidget {
-  ChewieDemo({this.title = 'Chewie Demo'});
-
   final String title;
+
+  ChewieDemo({this.title = 'Chewie Demo'});
 
   @override
   State<StatefulWidget> createState() {
-    return _ChewieDemoState();
+    return new _ChewieDemoState();
   }
 }
 
 class _ChewieDemoState extends State<ChewieDemo> {
   TargetPlatform _platform;
-  VideoPlayerController _videoPlayerController1;
-  VideoPlayerController _videoPlayerController2;
-  ChewieController _chewieController;
+  VideoPlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    _videoPlayerController1 = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController1,
-      aspectRatio: 3 / 2,
-      autoPlay: true,
-      looping: true,
+    _controller = new VideoPlayerController.network(
+      'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
     );
   }
 
   @override
-  void dispose() {
-    _videoPlayerController1.dispose();
-    _chewieController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: widget.title,
-      theme: ThemeData.light().copyWith(
+      theme: new ThemeData.light().copyWith(
         platform: _platform ?? Theme.of(context).platform,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Chewie(
-            controller: _chewieController,
+      home: new Scaffold(
+        body: new SafeArea(
+          child: new Chewie(
+            _controller,
+            aspectRatio: 3 / 2,
+            autoPlay: true,
+            looping: false,
           ),
+        )
         ),
-      ),
     );
   }
 }
